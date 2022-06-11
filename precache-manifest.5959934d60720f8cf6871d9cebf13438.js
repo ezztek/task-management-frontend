03571aa406936a8118a20186e1a6fb14868aa5b2
ezztek <ezztek@users.noreply.github.com>
2022-06-11 16:13:16 +0000

    deploy: 2dd663ce11e987b859f64e5d0babdd6feb0784d9

---
+++ precache-manifest.5959934d60720f8cf6871d9cebf13438.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c9704405d3bd45582dd4049e7b9497",
+    "url": "https://ezztek.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "3e0a813bf5db589c11c7",
+    "url": "https://ezztek.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "765e00fd7d8c6c8700f3",
+    "url": "https://ezztek.github.io/task-management-frontend/static/js/2.fcbd53eb.chunk.js"
+  },
+  {
+    "revision": "3e0a813bf5db589c11c7",
+    "url": "https://ezztek.github.io/task-management-frontend/static/js/main.fe3b22d9.chunk.js"
+  },
+  {
+    "revision": "37ca0ebab5d7f1cb7b5b",
+    "url": "https://ezztek.github.io/task-management-frontend/static/js/runtime~main.0f1654a7.js"
+  }
+]);
